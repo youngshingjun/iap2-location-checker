05097e5e1ed6935880413f57a9a0574ab20abf0f
supergo <supergo@g.cn>
2019-08-09 23:04:57 +0800

    init

---
+++ LocationCheckerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocationCheckerDemo
+//
+//  Created by supergo on 2020/6/4.
+//  Copyright © 2020 supergo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
